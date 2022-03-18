a36fbbf96c48a3befcd8f2d24961308196c82faa
GANESHCODE12 <balarama10@gmail.com>
2022-03-18 12:46:20 -0500

    Se crea pagina web  con componentes audiovisuales y plugin de autoplay

---
+++ assets/MediaPlayer.js
@@ -0,0 +1,44 @@
+class MediaPlayer {
+    constructor(config) {
+        this.media = config.el;
+        this.plugins = config.plugins || [];
+        this._initPlugins();
+    }
+    _initPlugins(){
+        this.plugins.forEach(plugin => {
+            plugin.run(this);
+        });
+    }
+    play() {
+        this.media.play();
+    }
+    pause() {
+        this.media.pause();
+    }
+    togglePlay() {
+        if(this.media.paused) {
+            document.querySelector('#playButton').innerHTML = '<i class="fa fa-pause-circle-o fa-lg " style="color: white;"  aria-hidden="true"></i>'
+            this.play();
+        } else {
+            document.querySelector('#playButton').innerHTML = '<i class="fa fa-play-circle fa-lg" style="color: white;"  aria-hidden="true"></i>'
+            this.pause()
+        }
+    }
+    mute() {
+        this.media.muted = true;
+    }
+    unmute() {
+        this.media.muted = false;
+    }
+    toggleMute() {
+        if(this.media.muted) {
+            document.querySelector('#muteButton').innerHTML = '<i class="fa fa-volume-up fa-lg" style="color: white;" aria-hidden="true"></i>'
+            this.unmute();
+        } else {
+            document.querySelector('#muteButton').innerHTML = '<i class="fa fa-volume-off fa-lg" style="color: white;" aria-hidden="true"></i>'
+            this.mute()
+        }
+    }
+}
+
+export default MediaPlayer;
---
+++ plugins/AutoPlay.js
@@ -0,0 +1,11 @@
+class AutoPlay {
+    constructor() {
+
+    }
+    run(player) {
+        player.mute();
+        player.play();
+    }
+}
+
+export default AutoPlay;
